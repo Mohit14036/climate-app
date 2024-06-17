42e857745728789ae9835e5bab84b22f7c4b8433
mohit14 <mohitjagini@gmail.com>
2024-06-17 22:31:03 +0530

    Profile Page

---
+++ lib/user_profile_screen.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:climate_app/main_weather_screen.dart';
+
+class UserProfileScreen extends StatefulWidget {
+  @override
+  _UserProfileScreenState createState() => _UserProfileScreenState();
+}
+
+class _UserProfileScreenState extends State<UserProfileScreen> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _locationController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadProfile();
+  }
+
+  _loadProfile() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _nameController.text = prefs.getString('name') ?? '';
+      _emailController.text = prefs.getString('email') ?? '';
+      _locationController.text = prefs.getString('location') ?? '';
+    });
+  }
+
+  _saveProfile() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('name', _nameController.text);
+    prefs.setString('email', _emailController.text);
+    prefs.setString('location', _locationController.text);
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Profile Saved')));
+
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => MainWeatherScreen()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // Dismiss the keyboard when tapping outside of text fields
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.asset(
+                    'icons/icon.jpg', // Replace this with the path to your image asset
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
+                  SizedBox(height: 20),
+                  Center(
+                    child: Text(
+                      'Weather App',
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Ubuntu',
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black, width: 2.0),
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Colors.white, // Background color for the container
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+                      child: TextField(
+                        controller: _nameController,
+                        decoration: InputDecoration(
+                          labelText: 'Name',
+                          border: InputBorder.none,
+                          labelStyle: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                        ),
+                        style: TextStyle(fontSize: 25),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black, width: 2.0),
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Colors.white, // Background color for the container
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+                      child: TextField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          border: InputBorder.none,
+                          labelStyle: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                        ),
+                        style: TextStyle(fontSize: 25),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black, width: 2.0),
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Colors.white, // Background color for the container
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+                      child: TextField(
+                        controller: _locationController,
+                        decoration: InputDecoration(
+                          labelText: 'Home Location',
+                          border: InputBorder.none,
+                          labelStyle: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                        ),
+                        style: TextStyle(fontSize: 25),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Center(
+                    child: Container(
+                      width: double.infinity, // Button width
+                      child: ElevatedButton(
+                        onPressed: _saveProfile,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue, // Button color
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(vertical: 16.0),
+                          child: Text('Save Profile', style: TextStyle(fontSize: 20,
+                          color: Colors.white)),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
